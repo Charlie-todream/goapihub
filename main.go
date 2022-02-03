e7a663a1aa87ffbdd343c046e289885b5144dae2
endlife93 <475181585@qq.com>
2022-02-03 19:16:59 +0800

    路由未定义

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 	"strings"
 )
 
-func main()  {
+func main() {
 
 	// 初始化Gin 实例
 	r := gin.Default()
 
 	// 注册中间件
-	r.Use(gin.Logger(),gin.Recovery())
+	r.Use(gin.Logger(), gin.Recovery())
 
 	// 注册一个路由
 	r.GET("/", func(c *gin.Context) {
 		// 以JSON 格式响应
-		c.JSON(http.StatusOK,gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"Hello": "World!",
 		})
 	})
@@ -26,11 +26,13 @@ func main()  {
 	r.NoRoute(func(c *gin.Context) {
 		// 获取标头信息的 Accept 信息
 		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString,"text/html") {
+		if strings.Contains(acceptString, "text/html") {
 			// 如果是HTML的话
-			c.JSON(http.StatusNotFound,gin.H{
-				"error_code":404,
-				"error_message":"路由未定义,请确认url和请求方法是否正确.",
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义,请确认url和请求方法是否正确.",
 			})
 		}
 	})
