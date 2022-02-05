340fc7ae2bb6f1f0cebe9f2930ae92f4b5d3396c
endlife93 <475181585@qq.com>
2022-02-05 17:11:00 +0800

    Email 是否已经注册了接口

--- app/http/controllers/api/v1/signup_controller.go
+++ app/http/controllers/api/v1/signup_controller.go
@@ -15,10 +15,10 @@ type  SignupController struct {
 // 检验手机是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context)  {
 	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	requst := PhoneExistRequest{}
+	//type PhoneExistRequest struct {
+	//	Phone string `json:"phone"`
+	//}
+	requst := requests.SignupPhoneExistRequest{}
 
 	// 解析Json请求
 	if err := c.ShouldBindJSON(&requst); err != nil {
@@ -45,5 +45,36 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context)  {
 
 	c.JSON(http.StatusOK,gin.H{
 		"exist":user.IsPhoneExist(requst.Phone),
+	})
+}
+
+// 验证邮箱是否已经注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+	// 初始化请求对象
+	request := requests.SignupEmailExistRequest{}
+
+	// 解析Json请求对象
+	if err := c.ShouldBindJSON(&request);err != nil {
+		// 解析失败 返回422错误
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity,gin.H{
+			"error":err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		return
+	}
+	// 表单验证
+	errs := requests.ValidateSignupEmailExist(&request,c)
+	if len(errs) > 0 {
+		// 验证失败 返回422
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity,gin.H{
+			"errors":errs,
+		})
+		return
+	}
+
+	// 检查数据库并返回响应
+	c.JSON(http.StatusOK,gin.H{
+		"exist":user.IsEmailExist(request.Email),
 	})
 }
--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -35,3 +35,36 @@ func ValidateSignupPhoneExist(data interface{},c *gin.Context) map[string][]stri
 	// 开始验证
 	return govalidator.New(opts).ValidateStruct()
 }
+
+ type SignupEmailExistRequest struct {
+ 	Email string `json:"email,omitempty" valid:"email"`
+ }
+
+func ValidateSignupEmailExist(data interface{},c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"email":[]string{"required","min:4","max:30","email"},
+	}
+	// 自定义验证出错时的提示
+	messages := govalidator.MapData{
+		"email":[]string{
+			"required:Email 必须填写",
+			"min:Email 长度需要大于4",
+			"max:Email 长度需要小于30",
+			"email:Email 格式不正确,请提供有效的邮箱地址",
+		},
+	}
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+		Messages:      messages,
+	}
+
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+
+}
+
+
