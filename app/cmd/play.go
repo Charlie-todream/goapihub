37ec74be409c2650d4e8e8b7cc0d1b928cd963ff
endlife93 <475181585@qq.com>
2022-02-19 19:55:14 +0800

    paly命令

---
+++ app/cmd/play.go
@@ -0,0 +1,17 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var CmdPlay = &cobra.Command{
+	Use : "play",
+	Short: "Likes the Go Playground, but running at our application context",
+	Run: nil,
+}
+// 调试完成后请记得清楚猜测代码
+func runPlay(cmd *cobra.Command,args []string)  {
+	// 存进去 redis 中
+	redis.Redis.Set("hello", "hi from redis", 10*time.Second)
+	// 从 redis 里取出
+	console.Success(redis.Redis.Get("hello"))
+}
+
