115a6bea04278efafc303b96f2438ae785f9fae1
endlife93 <475181585@qq.com>
2022-02-28 21:05:35 +0800

    验证分页请求

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -3,6 +3,7 @@ package v1
 import (
     "github.com/gin-gonic/gin"
     "goapihub/app/models/user"
+    "goapihub/app/requests"
     "goapihub/pkg/auth"
     "goapihub/pkg/response"
 )
@@ -17,6 +18,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context)  {
 }
 
 func (ctrl *UsersController) Index(c *gin.Context)  {
+
+    request := requests.PaginationRequest{}
+    if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+        return
+    }
 
     data, pager := user.Paginate(c, 10)
     response.JSON(c, gin.H{
