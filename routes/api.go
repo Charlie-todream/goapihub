600a95f0058b0e14e995be3a8032d262c6dce6ab
endlife93 <475181585@qq.com>
2022-03-06 21:06:13 +0800

    修改邮箱

--- routes/api.go
+++ routes/api.go
@@ -81,11 +81,12 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			linksGroup.GET("", lsc.Index)
 		}
-
+        uc := new(auth.UsersController)
 		usersGroup := v1.Group("/users")
 		{
 			usersGroup.GET("", uc.Index)
 			usersGroup.PUT("", middlewares.AuthJWT(), uc.UpdateProfile)
+			usersGroup.PUT("/email", middlewares.AuthJWT(), uc.UpdateEmail)
 		}
 	}
 }
